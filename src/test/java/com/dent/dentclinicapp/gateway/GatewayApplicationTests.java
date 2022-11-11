0cc9208f8173fe7f1fb12bcee627d660dc728c2c
Albert Heesch <oddajgrzanki@gmail.com>
2022-11-11 01:06:14 +0100

    first commit

---
+++ src/test/java/com/dent/dentclinicapp/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dent.dentclinicapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
